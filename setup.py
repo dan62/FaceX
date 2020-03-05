da401a63035ce257b941866d3b23b3c48eb58d8c
Daniel Goncalves <danielgoncalves62@gmail.com>
2020-03-05 22:51:53 +0700

    omit fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="FaceX-YOUR-USERNAME-HERE", # Replace with your own username
+    name="FaceX",
     version="0.0.1",
     author="Daniel Goncalves",
     author_email="danielgoncalves62@gmail.com",
